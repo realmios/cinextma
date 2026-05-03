47307cb75989b6fd23cb71c063a092ea97ef618a
Ngoc Hai <user.ngochai@gmail.com>
2026-05-03 10:23:44 +0700

    Thay đổi logo và caption

--- src/config/site.tsx
+++ src/config/site.tsx
@@ -15,35 +15,35 @@ import { TbFolder, TbFolderFilled } from "react-icons/tb";
 
 export const siteConfig: SiteConfigType = {
   name: "Cinextma",
-  description: "Your only choice for a free movies and tv shows streaming website.",
+  description: "Lựa chọn duy nhất của bạn cho website xem phim và chương trình TV miễn phí.",
   favicon: "/favicon.ico",
   navItems: [
     {
-      label: "Home",
+      label: "Trang chủ",
       href: "/",
       icon: <GoHome className="size-full" />,
       activeIcon: <GoHomeFill className="size-full" />,
     },
     {
-      label: "Discover",
+      label: "Khám phá",
       href: "/discover",
       icon: <IoCompassOutline className="size-full" />,
       activeIcon: <IoCompass className="size-full" />,
     },
     {
-      label: "Search",
+      label: "Tìm kiếm",
       href: "/search",
       icon: <BiSearchAlt2 className="size-full" />,
       activeIcon: <BiSolidSearchAlt2 className="size-full" />,
     },
     {
-      label: "Library",
+      label: "Thư viện",
       href: "/library",
       icon: <TbFolder className="size-full" />,
       activeIcon: <TbFolderFilled className="size-full" />,
     },
     {
-      label: "About",
+      label: "Giới thiệu",
       href: "/about",
       icon: <IoInformationCircleOutline className="size-full" />,
       activeIcon: <IoInformationCircle className="size-full" />,
@@ -66,69 +66,69 @@ export const siteConfig: SiteConfigType = {
   queryLists: {
     movies: [
       {
-        name: "Today's Trending Movies",
+        name: "Phim thịnh hành hôm nay",
         query: () => tmdb.trending.trending("movie", "day"),
         param: "todayTrending",
       },
       {
-        name: "This Week's Trending Movies",
+        name: "Phim thịnh hành tuần này",
         query: () => tmdb.trending.trending("movie", "week"),
         param: "thisWeekTrending",
       },
       {
-        name: "Popular Movies",
+        name: "Phim phổ biến",
         query: () => tmdb.movies.popular(),
         param: "popular",
       },
       {
-        name: "Now Playing Movies",
+        name: "Phim đang chiếu",
         query: () => tmdb.movies.nowPlaying(),
         param: "nowPlaying",
       },
       {
-        name: "Upcoming Movies",
+        name: "Phim sắp chiếu",
         query: () => tmdb.movies.upcoming(),
         param: "upcoming",
       },
       {
-        name: "Top Rated Movies",
+        name: "Phim đánh giá cao",
         query: () => tmdb.movies.topRated(),
         param: "topRated",
       },
     ],
     tvShows: [
       {
-        name: "Today's Trending TV Shows",
+        name: "TV thịnh hành hôm nay",
         query: () => tmdb.trending.trending("tv", "day"),
         param: "todayTrending",
       },
       {
-        name: "This Week's Trending TV Shows",
+        name: "TV thịnh hành tuần này",
         query: () => tmdb.trending.trending("tv", "week"),
         param: "thisWeekTrending",
       },
       {
-        name: "Popular TV Shows",
-        // @ts-expect-error: Property 'adult' is missing in type 'PopularTvShowResult' but required in type 'TV'.
+        name: "Chương trình TV phổ biến",
+        // @ts-expect-error
         query: () => tmdb.tvShows.popular(),
         param: "popular",
       },
       {
-        name: "On The Air TV Shows",
-        // @ts-expect-error: Property 'adult' is missing in type 'OnTheAirResult' but required in type 'TV'.
+        name: "Chương trình đang phát sóng",
+        // @ts-expect-error
         query: () => tmdb.tvShows.onTheAir(),
         param: "onTheAir",
       },
       {
-        name: "Top Rated TV Shows",
-        // @ts-expect-error: Property 'adult' is missing in type 'TopRatedTvShowResult' but required in type 'TV'.
+        name: "Chương trình TV đánh giá cao",
+        // @ts-expect-error
         query: () => tmdb.tvShows.topRated(),
         param: "topRated",
       },
     ],
   },
   socials: {
-    github: "https://github.com/wisnuwirayuda15/cinextma",
+    github: "https://www.facebook.com/ngochaidz",
   },
 };
 
