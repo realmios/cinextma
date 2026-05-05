68a37d238ac39172e910ab562962131a2a603b23
Ngoc Hai <user.ngochai@gmail.com>
2026-05-05 10:15:11 +0700

    fix favicon with correct PNG

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,8 +22,15 @@ export const metadata: Metadata = {
   description: siteConfig.description,
   manifest: "/manifest.json",
   icons: {
-    icon: siteConfig.favicon,
-  },
+  icon: [
+    { url: "/icons/android/android-launchericon-192-192.png", sizes: "192x192", type: "image/png" },
+    { url: "/icons/android/android-launchericon-512-512.png", sizes: "512x512", type: "image/png" },
+  ],
+  apple: [
+    { url: "/icons/ios/180.png", sizes: "180x180", type: "image/png" },
+    { url: "/icons/ios/152.png", sizes: "152x152", type: "image/png" },
+  ],
+ },
   twitter: {
     card: "summary",
     title: {
